b605d31af6d698a2b68ead38805636ff3feed167
lucasgongora <lucasdgongora@gmail.com>
2023-11-03 22:20:13 -0300

    Primer Commit_Juego base_pre.prueba técnica

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,222 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Side { Left = -2, Middle = 0, Right = 2}
+
+public class PlayerController : MonoBehaviour                                                             
+{
+    private Transform myTransform;
+    private Animator animator;
+    private CharacterController _myCharacterController;
+    public CharacterController MyCharacterController { get => _myCharacterController; set => _myCharacterController = value; }
+    private PlayerCollision playerCollision;
+
+
+    private Side position;
+    private Vector3 motionVector;
+    [Header("Player Controller")]
+    [SerializeField] private float forwardSpeed;
+    [SerializeField] private float jumpPower;
+    [SerializeField] private float dodgeSpeed;
+
+    private float rollTimer;
+    private float newXPosition;
+    private float xPosition;
+    private float yPosition;
+
+    private int IdDodgeLeft = Animator.StringToHash("DodgeLeft");
+    private int IdDodgeRight = Animator.StringToHash("DodgeRight");
+    private int IdJump = Animator.StringToHash("Jump");
+    private int IdFall = Animator.StringToHash("Fall");
+    private int IdLanding = Animator.StringToHash("Landing");
+    private int IdRoll = Animator.StringToHash("Roll");
+    
+    public int IdStumbleLow { get => _IdStumbleLow; set => _IdStumbleLow = value; }
+    private int _IdStumbleLow = Animator.StringToHash("StumbleLow");
+    public int IdDeathLower { get => _IdDeathLower; set => _IdDeathLower = value; }
+    private int _IdDeathLower = Animator.StringToHash("DeathLower");
+    public int IdDeathBounce { get => _IdDeathBounce; set => _IdDeathBounce = value; }
+    private int _IdDeathBounce = Animator.StringToHash("DeathBounce");
+    public int IdDeathMovingTrain { get => _IdDeathMovingTrain; set => _IdDeathMovingTrain = value; }
+    private int _IdDeathMovingTrain = Animator.StringToHash("DeathMovingTrain");
+    public int IdDeathUpper { get => _IdDeathUpper; set => _IdDeathUpper = value; }
+    private int _IdDeathUpper = Animator.StringToHash("DeathUpper");
+    public int IdStumbleCornerLeft { get => _IdStumbleCornerLeft; set => _IdStumbleCornerLeft = value; }
+    private int _IdStumbleCornerLeft = Animator.StringToHash("StumbleCornerLeft");
+    public int IdStumbleCornerRight { get => _IdStumbleCornerRight; set => _IdStumbleCornerRight = value; }
+    private int _IdStumbleCornerRight = Animator.StringToHash("StumbleCornerRight");
+    public int IdStumbleSideRight { get => _IdStumbleSideRight; set => _IdStumbleSideRight = value; }
+    private int _IdStumbleSideRight = Animator.StringToHash("StumbleSideRight");
+    public int IdStumbleSideLeft { get => _IdStumbleSideLeft; set => _IdStumbleSideLeft = value; }
+    private int _IdStumbleSideLeft = Animator.StringToHash("StumbleSideLeft");
+
+    private int IdStumbleFall = Animator.StringToHash("StumbleFall");
+    private int IdStumbleOffLeft = Animator.StringToHash("StumbleOffLeft");
+    private int IdStumbleOffRight = Animator.StringToHash("StumbleOffRight");
+    
+    private bool swipeLet, swipeRight, swipeUp, swipeDown;
+    
+    [Header("Player States")]
+    [SerializeField] private bool isJumping;
+    public bool IsRolling { get => _isRolling; set => _isRolling = value; }
+    
+
+    [SerializeField] private bool _isRolling;
+    [SerializeField] private bool isGrounded;
+
+
+    void Start()
+    {
+        
+        myTransform = GetComponent<Transform>();
+        animator = GetComponent<Animator>();
+        _myCharacterController = GetComponent<CharacterController>();
+        playerCollision = GetComponent<PlayerCollision>();
+        position = Side.Middle;
+        yPosition = -7;
+    }
+
+    void Update()
+    {
+        GetSwipe();
+        SetPlayePosition();
+        MovePlayer();
+        Jump();
+        Roll();
+        isGrounded = _myCharacterController.isGrounded;
+    }
+
+    private void GetSwipe()
+    {
+        swipeLet = Input.GetKeyDown(KeyCode.LeftArrow);
+        swipeRight = Input.GetKeyDown(KeyCode.RightArrow);
+        swipeUp = Input.GetKeyDown(KeyCode.UpArrow);
+        swipeDown = Input.GetKeyDown(KeyCode.DownArrow);
+    }
+
+    private void SetPlayePosition()
+    {
+        if (swipeLet && !IsRolling)
+        {
+            if(position == Side.Middle)
+            {
+                UpdatePlayerXPosition(Side.Left);
+                SetPlayerAnimator(IdDodgeLeft,false);
+            } 
+            else if(position == Side.Right)
+            {
+                UpdatePlayerXPosition(Side.Middle);
+                SetPlayerAnimator(IdDodgeLeft, false);
+            }
+        }
+        else if (swipeRight && !IsRolling)
+        {
+            if (position == Side.Middle)
+            {
+                UpdatePlayerXPosition(Side.Right);
+                SetPlayerAnimator(IdDodgeRight, false);
+            }
+            else if (position == Side.Left)
+            {
+                UpdatePlayerXPosition(Side.Middle);
+                SetPlayerAnimator(IdDodgeRight, false);
+            }
+        }
+
+
+    }
+
+    private void UpdatePlayerXPosition(Side plPosition)
+    {
+        newXPosition = (int)plPosition;
+        position = plPosition;
+    }
+
+    public void SetPlayerAnimator(int id, bool isCrossFade, float fadeTime =  0.1f)
+    {
+        animator.SetLayerWeight(0, 1);
+        if (isCrossFade)
+        {
+            animator.CrossFadeInFixedTime(id, fadeTime);
+        }
+        else
+        {
+            animator.Play(id);
+        }
+        ResetCollision();
+    }
+
+    public void SetPlayerAnimatorWithLayer(int id)  
+    {
+        animator.SetLayerWeight(1, 1);
+        animator.Play(id);
+        ResetCollision();
+    }
+
+    private void ResetCollision()
+    {
+        Debug.Log(playerCollision.CollisionX.ToString() + " " + playerCollision.CollisionY.ToString() + " " + playerCollision.CollisionZ.ToString());
+        playerCollision.CollisionX = CollisionX.None;
+        playerCollision.CollisionY = CollisionY.None;
+        playerCollision.CollisionZ = CollisionZ.None;
+    }
+
+    private void MovePlayer()
+    {
+        motionVector = new Vector3(xPosition - myTransform.position.x, yPosition * Time.deltaTime, forwardSpeed*Time.deltaTime);
+        xPosition = Mathf.Lerp(xPosition, newXPosition, Time.deltaTime * dodgeSpeed);
+        _myCharacterController.Move(motionVector);
+    }
+
+    private void Jump()
+    {
+        if (_myCharacterController.isGrounded)
+        {
+            isJumping = false;
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName("Fall"))
+            {
+                SetPlayerAnimator(IdLanding, false);
+            }
+            if (swipeUp & !IsRolling)
+            {
+                isJumping = true;
+                yPosition = jumpPower;
+                SetPlayerAnimator(IdJump, true);
+            }
+        }
+        else
+        {
+            yPosition -= jumpPower * 2 * Time.deltaTime;
+            if (_myCharacterController.velocity.y <= 0)
+            {
+                SetPlayerAnimator(IdFall, false);
+            }
+            
+        }
+    }
+
+    private void Roll()
+    {
+        rollTimer -= Time.deltaTime;
+        if (rollTimer <= 0)
+        {
+            IsRolling = false;
+            rollTimer = 0;
+            // Poner ch tamaño normal
+            _myCharacterController.center = new Vector3(0, .45f, 0);
+            _myCharacterController.height = .9f;
+        }
+
+        if (swipeDown & !isJumping)
+        {
+            IsRolling = true;
+            rollTimer = .5f;
+            SetPlayerAnimator(IdRoll, true);
+            // Poner ch tamaño roll
+            _myCharacterController.center = new Vector3(0, .2f, 0);
+            _myCharacterController.height = .4f;
+        }
+    }
+}
